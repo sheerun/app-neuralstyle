c7fe3322b11facdfcb1718723625a85a0cf5a1f5
Stephen Fox <stephen.fox@nimbix.net>
2016-12-02 20:42:26 -0600

    Refactor arguments in stylize script

--- scripts/stylize.sh
+++ scripts/stylize.sh
@@ -8,6 +8,10 @@ if [ -r /etc/JARVICE/jobenv.sh ]; then
     . /etc/JARVICE/jobenv.sh
 fi
 
+STYLE_IMAGE="$1"
+CONTENT_IMAGE="$2"
+GPU_COUNT="$3"
+
 if [ $GPU_COUNT -lt 1 ]; then
     GPU_ARGS="-gpu -1"
 fi
@@ -17,4 +21,4 @@ mkdir -p $OUTDIR
 
 cd /usr/local/neural-style/neural-style
 
-exec th neural_style.lua  ${GPU_ARGS} -style_image "$1" -content_image "$2" -output_image "${OUTDIR}/${JOB_NAME}.png"
+exec th neural_style.lua  ${GPU_ARGS} -style_image ${STYLE_IMAGE} -content_image ${CONTENT_IMAGE} -output_image "${OUTDIR}/${JOB_NAME}.png"
