2aa64058ede0a504db75ec31543d91a38b842e07
Adam Stankiewicz <sheerun@sher.pl>
2017-04-07 18:53:09 +0200

    Use transfer.sh to processing

--- scripts/stylize.sh
+++ scripts/stylize.sh
@@ -14,16 +14,25 @@ GPU_COUNT="$3"
 EXTRA_ARGS="$4"
 
 if [ $GPU_COUNT -lt 1 ]; then
-    GPU_ARGS="-gpu -1"
+	GPU_ARGS="-gpu -1"
 fi
 
 OUTDIR=/data/NeuralStyle
-mkdir -p $OUTDIR
+mkdir -p "$OUTDIR/$JOB_NAME"
+
+curl -o "$OUTDIR/$JOB_NAME/style.jpg" "$STYLE_IMAGE"
+curl -o "$OUTDIR/$JOB_NAME/content.jpg" "$STYLE_IMAGE"
 
 cd /usr/local/neural-style/neural-style
 . /usr/local/torch/install/bin/torch-activate
 
+export OUTPUT_PATH="${OUTDIR}/${JOB_NAME}/output.jpg"
+
 exec th neural_style.lua ${GPU_ARGS} "${EXTRA_ARGS}" \
   -style_image "${STYLE_IMAGE}" \
   -content_image "${CONTENT_IMAGE}" \
-  -output_image "${OUTDIR}/${JOB_NAME}.png" \
+  -output_image "${OUTPUT_PATH}"
+
+export OUTPUT_URL=$(curl --upload-file "${OUTPUT_PATH}" https://transfer.sh/output.jpg)
+
+echo "FINALRESULT:$OUTPUT_URL"
